854ce7dd664af337978dfe8e166c41d8ab3edb5b
Paulo Do Monte <55249641+PauloDoMonte@users.noreply.github.com>
2022-05-22 18:40:12 -0300

    Add files via upload

--- Desafio da Liga de Inteligência de Artificial/main.py
+++ Desafio da Liga de Inteligência de Artificial/main.py
@@ -43,7 +43,7 @@ clr.fit(x_train, y_train)
 resultado = clr.predict(df)
 df['resultado'] = resultado
 
-'''
+
 heatmap_data = pd.pivot(df,values='resultado',columns='expected_hours',index='price')
 
 ax = sb.heatmap(heatmap_data, cbar_kws={'label':'Unfinished','ticks':[0,1]})
@@ -51,21 +51,19 @@ plt.title("Relação entre preço por hora de trabalho no Mercado")
 plt.ylabel("Preço [$]")
 plt.xlabel("Horas de trabalho")
 plt.savefig("ieee.png")
-'''
-'''
+
 plt.title("Preco por horas trabalhadas aceito no Mercado")
 plt.scatter(df.loc[df['resultado']==1]['expected_hours'],df.loc[df['resultado']==1]['price'],color='blue',s=1)
 plt.ylabel("Preço [$]")
 plt.xlabel("Horas")
 plt.grid()
-plt.savefig("ieee2.png")'''
+plt.savefig("ieee2.png")
 
-'''
 plt.title("Preco por horas trabalhadas não aceito no Mercado")
 plt.scatter(df.loc[df['resultado']==0]['expected_hours'],df.loc[df['resultado']==0]['price'],color='blue',s=1)
 plt.ylabel("Preço [$]")
 plt.xlabel("Horas")
 plt.grid()
-plt.savefig("ieee3.png")'''
+plt.savefig("ieee3.png")
 
 print(resultado)
